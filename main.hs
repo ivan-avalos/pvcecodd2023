88eeff6f551087ab3b0878875cbfdb43754fe5af
Iván Ávalos <avalos@disroot.org>
2023-03-10 07:33:52 -0600

    Se añaden campos a tabla mantenimientos

--- main.hs
+++ main.hs
@@ -14,7 +14,7 @@ migrate conn = do
                 \ fecha_nacimiento TEXT NOT NULL, \
                 \ genero TEXT NOT NULL, \
                 \ departamento_id INTEGER, \
-                \ FOREIGN KEY (departamento_id) REFERENCES departamentos (id))"
+                \ FOREIGN KEY (departamento_id) REFERENCES departamentos (id) ON DELETE SET NULL)"
   -- Crear tabla «máquinas»
   execute_ conn "CREATE TABLE IF NOT EXISTS maquinas ( \
                 \ serie TEXT PRIMARY KEY, \
@@ -22,13 +22,16 @@ migrate conn = do
                 \ marca TEXT NOT NULL, \
                 \ usuario_id INTEGER, \
                 \ departamento_id INTEGER, \
-                \ FOREIGN KEY (usuario_id) REFERENCES usuario (id), \
-                \ FOREIGN KEY (departamento_id) REFERENCES departamentos (id))"
+                \ FOREIGN KEY (usuario_id) REFERENCES usuario (id) ON DELETE SET NULL, \
+                \ FOREIGN KEY (departamento_id) REFERENCES departamentos (id) ON DELETE SET NULL)"
   -- Crear tabla «mantenimientos»
   execute_ conn "CREATE TABLE IF NOT EXISTS mantenimientos ( \
                 \ id INTEGER PRIMARY KEY, \
                 \ tipo TEXT NOT NULL, \
-                \ fecha TEXT NOT NULL)"
+                \ fecha_creacion TEXT NOT NULL, \
+                \ servidor_id INTEGER, \
+                \ fecha_servicio TEXT, \
+                \ FOREIGN KEY (servidor_id) REFERENCES usuarios (id) ON DELETE SET NULL)"
   -- Crear tabla «materiales«
   execute_ conn "CREATE TABLE IF NOT EXISTS materiales ( \
                 \ id INTEGER PRIMARY KEY, \
@@ -39,8 +42,8 @@ migrate conn = do
                 \ mantenimiento_id INTEGER, \
                 \ material_id INTEGER, \
                 \ cantidad NUMERIC NOT NULL, \
-                \ FOREIGN KEY (mantenimiento_id) REFERENCES mantenimientos (id), \
-                \ FOREIGN KEY (material_id) REFERENCES materiales (id), \
+                \ FOREIGN KEY (mantenimiento_id) REFERENCES mantenimientos (id) ON DELETE CASCADE, \
+                \ FOREIGN KEY (material_id) REFERENCES materiales (id) ON DELETE CASCADE, \
                 \ PRIMARY KEY (mantenimiento_id, material_id))"
 
 main :: IO ()
